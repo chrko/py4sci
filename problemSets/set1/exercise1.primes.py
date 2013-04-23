22f7de8840e552382d5ba66c601e84f5e0198d79
Christian Kohlstedde <christian@kohlsted.de>
2013-04-23 21:54:49 +0200

    finished exercise 1

--- problemSets/set1/exercise1.primes.py
+++ problemSets/set1/exercise1.primes.py
@@ -4,8 +4,22 @@ Compute primes up to 1000
 (C) @ Christian Kohlstedde
 """
 
+# printing the 2 all times
+# just knowing her good enough
 print 2
-for prime in range(3,1000):
+
+# running for loop over the range from 3 to 1000 excluding the even numbers by submitting 2 as the step 
+for prime in range(3,1000,2):
+
+    # helper var
+    isprime = True
+
+    # running the test, starting a 3 to the testing number,
+    # also excluding the evens
     for test in range(3, prime, 2):
-        if prime % test !== 0:
-            print prime
+	# test
+        if (prime % test) == 0:
+	    isprime = False
+    
+    if isprime:
+	print prime
