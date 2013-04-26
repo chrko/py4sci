e8fdbaed93b235cb040d4d8055a79f8cb36f625c
Christian Kohlstedde <christian@kohlsted.de>
2013-04-26 12:12:58 +0200

    practice problem 1 solved

--- practice/pro1.factorial.py
+++ practice/pro1.factorial.py
@@ -3,15 +3,35 @@ doing some factorial stuff
 
 (c) Christian Kohlstedde
 """
+import math
+
+def unit_test_num(value1, value2):
+    test = value1 == value2
+    if test:
+        text = "check!"
+    else:
+        text = "something has gone wrong :o"
+    print value1, "=", value2, "=> " + text
 
 def factorial_iter(num):
     num = int(num)
     if num == 0:
-	return 1
+        return 1
     else:
-	fac = 1
-	for i in range(1, num + 1):
-	    fac *= i
-	return fac
+        fac = 1
+    for i in range(1, num + 1):
+        fac *= i
+    return fac
 
-print factorial_iter( 5 )
+def factorial_rec(num):
+    num = int(num)
+    if num > 1:
+        return num * factorial_rec(num - 1)
+    else:
+        return 1
+
+print "iterative"
+unit_test_num(factorial_iter(5), math.factorial(5))
+
+print "recursive"
+unit_test_num(factorial_rec(5), math.factorial(5))
