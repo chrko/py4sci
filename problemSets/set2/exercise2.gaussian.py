10b42932fda26c8250d88118138bf0d2d8a6b99d
Christian Kohlstedde <christian@kohlsted.de>
2013-04-26 11:56:06 +0200

    Finished exercise 2 of problem set 2

--- problemSets/set2/exercise2.gaussian.py
+++ problemSets/set2/exercise2.gaussian.py
@@ -14,6 +14,11 @@ def gaussian(x, mu, sigma):
 print gaussian(1, 0.5, 0.63)
 
 def integrate_gaussian(mu, sigma, xmin, xmax, n_steps):
+    mu = float(mu)
+    sigma = float(sigma)
+    xmin = float(xmin)
+    xmax = float(xmax)
+    n_steps = int(n_steps)
     h = (xmax - xmin) / n_steps
     s = gaussian(xmin, mu, sigma) + gaussian(xmax, mu, sigma)
     for i in xrange(1, n_steps):
@@ -26,4 +31,18 @@ xmin = -10.
 xmax = +10.
 n_steps = 1000
 
-print integrate_gaussian(mu, sigma, xmin, xmax, n_steps)
+print "integrate_gaussian(mu = 3., sigma =1., xmin = -10., xmax = +10., n_steps) ", integrate_gaussian(mu, sigma, xmin, xmax, n_steps)
+
+# Decreasing the n_steps causes much less accurancy
+# => the used numerous integral depends on small steps in relation to the integrated function
+# so the resolution is great enough to recognize small changes in the graph
+
+print "n_steps at 300 ", integrate_gaussian(mu, sigma, xmin, xmax, 300)
+print "n_steps at 100 ", integrate_gaussian(mu, sigma, xmin, xmax, 100)
+print "n_steps at 30 ", integrate_gaussian(mu, sigma, xmin, xmax, 30)
+print "n_steps at 10 ", integrate_gaussian(mu, sigma, xmin, xmax, 10)
+print "n_steps at 3 ", integrate_gaussian(mu, sigma, xmin, xmax, 3)
+
+# The mu is 3, so setting the xmin also to 3
+# gives only the a small area of the wanted graph
+print "xmin = 3.", integrate_gaussian(mu, sigma, 3., xmax, 3)
