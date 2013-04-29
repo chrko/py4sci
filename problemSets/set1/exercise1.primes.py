533b453097ec6904f7855fb5b9718bc4c1660963
Christian Kohlstedde <christian@kohlsted.de>
2013-04-29 11:14:32 +0200

    Adding mean calc, related to issue #2

--- problemSets/set1/exercise1.primes.py
+++ problemSets/set1/exercise1.primes.py
@@ -7,9 +7,17 @@ how to run:
 python exercise1.prime.py
 """
 
+# initiating some vars to calculate the mean
+# init sum
+sum = 0
+# init count
+count = 0
+
 # printing the 2 all times
 # just knowing her good enough
 print 2
+count += 1
+sum   += 2
 
 # running for loop over the range from 3 to 1000 excluding the even numbers by submitting 2 as the step
 for prime in range(3,1000,2):
@@ -27,3 +35,8 @@ for prime in range(3,1000,2):
 
     if isprime:
         print prime
+        # doing stuff to calc the mean
+        count += 1
+        sum   += prime
+
+print "Mean:", (sum / float(count))
