22ca9c429f366ecfe4aa9675021d7a66bcc8d0c9
Christian Kohlstedde <christian@kohlsted.de>
2013-05-13 18:00:50 +0200

    just working

--- problemSets/set4/exoplanet.py
+++ problemSets/set4/exoplanet.py
@@ -22,28 +22,38 @@ width = 1
 
 def median_filter(data, width):
     for position in range(len(data)):
-        cur_x = data[position, 0]
-        new_y = 0
-        count = 0
-        for x in data[position:, 0]:
-            if (x - cur_x) < (width / 2):
-                new_y += data[position + count, 1]
-                count += 1
-            else:
-                break
-        new_y /= count
-        count = 1
-        for x in data[position::-1, 0]:
-            if x == cur_x:
-                continue
-            if (cur_x - x) < (width / 2):
-                new_y += data[position - count, 1]
-                count += 1
-            else:
-                break
-        new_y /= count
-        data[position, 1] = new_y
+        data[position, 1] = np.mean(
+            data[abs(data[:, 0] - data[position, 0]) < 1, 1]
+        )
+
+
+median_filter(data, 1)
 
 plt.cla()
-plt.plot(dataOrig[:,0], dataOrig[:,1])
-plt.plot(data[:,0], data[:,1])
+plt.scatter(dataOrig[:, 0], dataOrig[:, 1], s=1)
+plt.plot(data[:, 0], data[:, 1], 'r', lw=2)
+
+
+residualDataY = abs(dataOrig[:, 1] - data[:, 1]) 
+plt.cla()
+plt.plot(data[:, 0], residualDataY)
+
+
+sense = 10
+dt = np.diff(data[residualDataY > np.mean(residualDataY) * sense, 0])
+period = np.mean(dt[(dt > 1) & (dt < 15)])
+
+
+plt.cla()
+phaseX = ((data[:, 0]) % period) / period
+phaseX = ((data[:, 0] - np.mean(phaseX[residualDataY > np.mean(residualDataY) * sense] - 0.5) * period) % period) / period
+
+
+
+plt.plot(phaseX, residualDataY)
+
+plt.cla()
+
+extract = (phaseX > 0.45) & (phaseX < 0.55)
+plt.plot(phaseX[extract], residualDataY[extract])
+plt.axis([0.45, 0.55, plt.axis()[2], plt.axis()[3]])
