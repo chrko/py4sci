45b7cf898f873cd26c1a2414f3ee57a6a1742ad1
Christian Kohlstedde <christian@kohlsted.de>
2013-05-27 19:43:52 +0200

    adding some comments and updated task 4

--- problemSets/set5/radioactive.py
+++ problemSets/set5/radioactive.py
@@ -3,20 +3,27 @@ radioactive - not by The Imagine Dragons
 
 2013 (c) Christian Kohlsteddde
 """
-
 import numpy as np
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-plt.ion()
+#plt.ion()
 
+# Loading data from decay_data.txt and split into shorter names
 data = np.loadtxt('decay_data.txt')
 x = data[:, 0]
 y = data[:, 1]
 yerr = data[:, 2]
 
+# plotting the errorbar
+plt.cla()
+plt.errorbar(x, y, yerr=yerr, fmt=None)
+plt.plot(x, y)
+plt.savefig('error.png')
 
+
+# defining the first try of decay
 def radioactive(t, r_0, tau):
     return r_0 * np.exp(-t / tau)
 
@@ -27,12 +34,14 @@ plt.cla()
 plt.errorbar(x, y, yerr=yerr, fmt=None)
 plt.plot(x, y)
 plt.plot(x, y_line)
+plt.savefig("radioactive.png")
 
 print "radioactive = r_0 * np.exp(-t / tau)"
 print "tau:", popt[1], "+/-", pcov[1, 1]**0.5
 print "r_0:", popt[0], "+/-", pcov[0, 0]**0.5
 
 
+# defining more real approximation
 def radioactive_real(t, r_0, tau, r_bkg):
     return r_0 * np.exp(-t / tau) + r_bkg
 
@@ -43,6 +52,7 @@ plt.cla()
 plt.errorbar(x, y, yerr=yerr, fmt=None)
 plt.plot(x, y)
 plt.plot(x, y_line_real)
+plt.savefig("radioactive_real.png")
 
 print ""
 print "radioactive_real = r_0 * np.exp(-t / tau) + r_bkg"
@@ -51,11 +61,12 @@ print "r_0:\t", popt_real[0], "\t+/-", pcov_real[0, 0]**0.5
 print "r_bkg:\t", popt_real[2], "\t+/-", pcov_real[2, 2]**0.5
 
 
-def chi(y, m, e):
+# the chi
+def chi(y, m, e, p):
     return np.sum(
         ((y - m) / e)**2
-    ) / len(y)
+    ) / (len(y) - p - 1)
 
 print ""
-print "chi for radioactive", chi(y, y_line, yerr)
-print "chi for radioactive_real", chi(y, y_line_real, yerr)
+print "chi for radioactive\t\t", chi(y, y_line, yerr, 2)
+print "chi for radioactive_real\t", chi(y, y_line_real, yerr, 3)
